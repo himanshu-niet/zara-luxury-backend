4ef8804a05caf543e20eb0f1865594f3c6ae09a0
Himanshu <108611598+himanshu-niet@users.noreply.github.com>
2023-10-26 08:18:41 +0000

    update 26

---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,14 @@
+// Create Token
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJWTToken();
+
+ 
+  res.status(statusCode).json({
+    success: true,
+    user,
+    token,
+  });
+};
+
+module.exports = sendToken;
